7cb37672eddb98742d136858a4e9314857ca35b9
zhafiraak <zhafirakhafianti19@gmail.com>
2022-01-11 21:50:03 +0700

    menambahkan file view

---
+++ view/input_nilai.py
@@ -0,0 +1,38 @@
+# Menginput data
+def input_nama():
+    print("\nMasukkan data mahasiswa")
+    print("...")
+    global nama
+    nama = input("\nNama: ")
+    return nama
+
+
+def input_nim():
+    global nim
+    nim = input("NIM: ")
+    return nim
+
+
+def input_ntugas():
+    global nilai_tugas
+    nilai_tugas = int(input("Masukkan nilai tugas: "))
+    return nilai_tugas
+
+
+def input_nuts():
+    global nilai_uts
+    nilai_uts = int(input("Masukkan nilai UTS: "))
+    return nilai_uts
+
+
+def input_nuas():
+    global nilai_uas
+    nilai_uas = int(input("Masukkan nilai UAS: "))
+    return nilai_uas
+
+
+# Nilai akhir
+def nakhir():
+    global nilai_akhir
+    nilai_akhir = (nilai_tugas)*30/100 + (nilai_uts)*35/100 + (nilai_uas)*35/100
+    return nilai_akhir
---
+++ view/view_nilai.py
@@ -0,0 +1,18 @@
+# Menampilkan data
+from model.daftar_nilai import data
+
+def lihat_data():
+    print("Daftar Nilai:")
+    print("===================================================================")
+    print("| No |      Nama      |    NIM    | Tugas |  UTS  |  UAS  | Akhir |")
+    print("===================================================================")
+    if data.keys():
+        no = 1
+        for tabel in data.values():
+            print("| {0:2} | {1:14} | {2:9} | {3:5} | {4:5} | {5:5} | {6:5} |".format
+                (no, tabel[0], tabel[1], tabel[2], tabel[3], tabel[4], tabel[5]))
+            print('-------------------------------------------------------------------')
+            no += 1
+    else:
+        print("=========================TIDAK ADA DATA============================")
+        print("===================================================================")
